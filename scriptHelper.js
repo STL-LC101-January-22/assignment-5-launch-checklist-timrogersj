c4e92192363f51ca4d79ad296d0957d95d9a8b38
Your Name~ <timrogersj@gmail.com>
2022-03-29 18:02:25 -0500

    changes

--- scriptHelper.js
+++ scriptHelper.js
@@ -62,7 +62,7 @@ function formSubmission(document, list, pilotName, copilotName, fuelLevel, cargo
 
 
         launchStatus = document.getElementById('launchStatus');
-            list.style.visibility = 'hidden';
+            faultyItems.style.visibility = 'hidden';
             pilotStatus.innerHTML = `Pilot ${pilotName} is ready for launch`;
             copilotStatus.innerHTML = `Co-pilot ${copilotName} is ready for launch`;
             fuelStatus.innerHTML = "Fuel level high enough for launch";
